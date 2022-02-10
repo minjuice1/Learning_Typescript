d85131bfa85b910205bb50b94b1fda48f1390edf
minjuice1 <minjuice93@gmail.com>
2022-02-10 19:22:11 +0900

    함수에 사용하는 never 타입

---
+++ codingapple_ts/part2/part2_03.ts
@@ -0,0 +1,12 @@
+// function return 값에 붙일 수 있는 never type
+// 아무것도 return 하지 않을 때 사용
+
+// 조건 1. return 값이 없어야 함.
+// 조건 2. endpoint가 없어야 함.
+
+// never 타입 등장하는 경우
+// 1. 이상한 narrowing
+// 2. 어떤 함수표현식은 return 타입이 자동으로 never
+
+// never 타입은 쓸 일은 없고 등장하면 이해만 하면 됨
+// 그럴일이 없다, 코드가 뭔가 좀 이상하다는 느낌인거임.
