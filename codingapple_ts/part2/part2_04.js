e6a4a1b1494433990efe7b8f3a929f7bfd7324a4
minjuice1 <minjuice93@gmail.com>
2022-02-13 19:22:11 +0900

    public, private 키워드 사용

---
+++ codingapple_ts/part2/part2_04.js
@@ -0,0 +1,9 @@
+var User = /** @class */ (function () {
+    function User(a) {
+        this.name = 'kim';
+        this.name = a;
+    }
+    return User;
+}());
+var 유저1 = new User('park');
+유저1.name = 'hi';
---
+++ codingapple_ts/part2/part2_04.ts
@@ -0,0 +1,10 @@
+class User {
+  private name = 'kim';
+  constructor(a: string) {
+    this.name = a
+  }
+}
+
+let 유저1 = new User('park');
+유저1.name = 'hi';
+
