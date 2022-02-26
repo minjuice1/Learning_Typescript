d919125219cfaf6916639c3186bd94a202580508
minjuice1 <minjuice93@gmail.com>
2022-02-26 14:31:11 +0900

    react_ts 초기세팅

---
+++ codingapple_react_ts/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h4>Hello</h4>
+    </div>
+  );
+}
+
+export default App;
