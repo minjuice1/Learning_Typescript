19523bc36a1e4660960ec7a2e2a03fda68d6cdf0
minjuice1 <minjuice93@gmail.com>
2022-03-03 16:04:11 +0900

    d.ts 파일 이용하기

--- codingapple_ts/part2/part2_09.d.ts
+++ codingapple_ts/part2/part2_09.d.ts
@@ -4,5 +4,8 @@
 // 1. ts파일에 타입정의가 너무 길면 d.ts 파일을 만들어서 간결화.
 // 2. 모든 타입을 정리해놓은 레퍼런스용으로 d.ts을 사용하기도 함.
 
+// d.ts파일은 자동으로 글로벌 모듈 아님 => tsconfig.json에 설정
+
 export type AgeType = number;
 export interface PersonType {TestName: string}
+type TestType = number;
---
+++ codingapple_ts/part2/part2_09.ts
@@ -0,0 +1,9 @@
+// 코드 간결화를 위해 d.ts에서 정의한 타입들을 불러와서 사용.
+// import할게 너무 많으면은
+// import * as a from './part2_09.d';
+
+import { AgeType, PersonType } from './part2_09.d';
+
+let age :AgeType = 20;
+
+console.log(age);
