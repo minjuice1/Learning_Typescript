d804f9cfa8167fca7258da2d45cec361a7e3b105
minjuice1 <minjuice93@gmail.com>
2022-03-06 17:34:11 +0900

    조건문으로 타입만들기 & infer 숙제

--- codingapple_ts/part2/part2_13.js
+++ codingapple_ts/part2/part2_13.js
@@ -3,3 +3,7 @@ var a13;
 var a13_1;
 var age13_1;
 var age13_2;
+var age1;
+var age2;
+var type13_1; //이러면 number가 이 자리에 남음
+var type13_2; //이러면 string이 이 자리에 남음
--- codingapple_ts/part2/part2_13.ts
+++ codingapple_ts/part2/part2_13.ts
@@ -23,3 +23,21 @@ type a = 타입추출< ()=> void > // T는 ()=>void 형태로 되고, infer는 �
 
 // 근데 함수의 return 타입을 알고싶을 때 쓰는 함수는 따로 있다. => ReturnType이라는 기본 함수 사용.
 type b = ReturnType< () => void >
+
+
+// (숙제1) 타입 파라미터로
+// 1. array 타입을 입력하면
+// 2. array의 첫 자료가 string이면 string 타입을 그대로 남겨주고 
+// 3. array의 첫 자료가 string이 아니면 unknown 을 남겨주려면 어떻게 타입을 만들어놔야할까요?
+
+type Age13_1<T> = T extends [string, ...any] ? T[0] : unknown;
+
+let age1 :Age13_1<[string, number]>;
+let age2 :Age13_1<[boolean, number]>; 
+
+// (숙제2) 함수의 파라미터의 타입을 뽑아주는 기계를 만들어보십시오. 
+
+type 타입뽑기<T> = T extends (x: infer R) => any ? R : any;
+
+let type13_1: 타입뽑기<(x :number) => void> //이러면 number가 이 자리에 남음
+let type13_2: 타입뽑기<(x :string) => void> //이러면 string이 이 자리에 남음
